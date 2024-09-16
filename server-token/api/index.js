bec3776e2c1ddf664cc9fea4c1b6b25167ebb5ef
aympe <59691588+aympe@users.noreply.github.com>
2024-09-16 10:37:26 +0900

    Update index.js

--- server-token/api/index.js
+++ server-token/api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-const { RtcTokenBuilder2, Role } = require('./RtcTokenBuilder2'); // Import Role from RtcTokenBuilder2.js
+const { RtcTokenBuilder, Role } = require("./RtcTokenBuilder2"); // Import Role from RtcTokenBuilder2.js
   // Path to RtcTokenBuilder2.js in the same folder
 
 require("dotenv").config();
