b61cce32a7bdd7e4d91c5b9395b629293359be11
aympe <59691588+aympe@users.noreply.github.com>
2024-09-16 15:19:47 +0900

    e

--- server-token/api/index.js
+++ server-token/api/index.js
@@ -203,7 +203,7 @@ app.post("/start", async (req, res) => {
     );
 
     const response = await axios.post(
-      `https://api.agora.io/v1/apps/${process.env.APP_ID}/cloud_recording/resourceid/${resourceId}/mode/composite/start`,
+      `https://api.agora.io/v1/apps/${process.env.APP_ID}/cloud_recording/resourceid/${resourceId}/mode/individual/start`,
       payload,
       {
         headers: {
