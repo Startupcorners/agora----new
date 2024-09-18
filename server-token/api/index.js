da07166fa885728529464c4e182b76363a871850
aympe <59691588+aympe@users.noreply.github.com>
2024-09-18 14:41:34 +0900

    Update index.js

--- server-token/api/index.js
+++ server-token/api/index.js
@@ -184,7 +184,7 @@ app.post("/start", async (req, res) => {
               serviceName: "web_recorder_service",
               errorHandlePolicy: "error_abort",
               serviceParam: {
-                url: `"https://sccopy-38403.bubbleapps.io/video/1726195519465x346418864932257800?r=1721913797942x965183480405939000&isaws=yes"`,
+                url: `https://sccopy-38403.bubbleapps.io/video/1726195519465x346418864932257800?r=1721913797942x965183480405939000&isaws=yes`,
                 audioProfile: 1,
                 videoWidth: 1280,
                 videoHeight: 720,
