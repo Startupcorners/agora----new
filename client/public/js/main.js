3101e9ebbc7366350b4419e1ca3d770c6b29c3c9
aympe <59691588+aympe@users.noreply.github.com>
2024-09-24 14:07:54 +0900

    Update main.js

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -6,7 +6,7 @@
  * <script src="https://unpkg.com/agora-extension-virtual-background@1.2.0/agora-extension-virtual-background.js"></script>
  */
 
-import AgoraRTM from "agora-rtm-sdk";
+import AgoraRTM from "../../node_modules/agora-rtm-sdk/AgoraRTM.js";
 
 
 const templateVideoParticipant = `<div id="video-wrapper-{{uid}}" style="
