605a13f360591a499b6a1db2409f89c05df806b9
aympe <59691588+aympe@users.noreply.github.com>
2024-11-23 05:08:42 +0900

    Update main.js

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -300,6 +300,7 @@ const join = async () => {
         console.log("RTM members 2 or 3 detected. Event is being recorded.");
         bubble_fn_waitingForAcceptance(); // Trigger the Bubble function to display the popup
       }
+    bubble_fn_joining("Joined");
     }
   } catch (error) {
     console.error("Error during join:", error);
