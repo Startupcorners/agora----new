b2167bd65d3224e0b4d9d06e40db42b27d9b1774
aympe <59691588+aympe@users.noreply.github.com>
2024-12-03 12:16:42 +0900

    Update header.js

--- client/public/js/header.js
+++ client/public/js/header.js
@@ -1,7 +1,7 @@
 // External script imports
 import "https://cdn.jsdelivr.net/npm/agora-rtc-sdk-ng@4.22.2/AgoraRTC_N-production.min.js";
 import "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
-import "https://unpkg.com/agora-extension-virtual-background@1.3.0/agora-extension-virtual-background.js";
+import "https://unpkg.com/agora-extension-virtual-background@2.0.0/agora-extension-virtual-background.js";
 import "https://startupcorners-df3e7.web.app/js/main.js";
 
 // Inline JavaScript
