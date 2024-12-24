73d73b0ed24c93c7ec671d7d7ef32251575ba426
aympe <59691588+aympe@users.noreply.github.com>
2024-12-24 12:26:43 +0900

    Update schedule.js

--- client/public/js/schedule.js
+++ client/public/js/schedule.js
@@ -82,7 +82,7 @@ export const schedule = async function () {
       ).padStart(2, "0");
 
       // final "local" date-time string (always T00:00).
-      const dateStr = `${localYear}-${localMonth}-${localDay}T00:00${sign}${offsetHours}:${offsetMinutes}Z`;
+      const dateStr = `${localYear}-${localMonth}-${localDay}T00:00${sign}${offsetHours}:${offsetMinutes}`;
 
       dates.push(dateStr);
     }
