b65b7be3e22008dbda71473046741e60dff4a904
aympe <59691588+aympe@users.noreply.github.com>
2024-12-24 12:40:34 +0900

    Update schedule.js

--- client/public/js/schedule.js
+++ client/public/js/schedule.js
@@ -89,7 +89,7 @@ export const schedule = async function () {
 
     // 10) Return or bubble
     console.log(dates);
-    bubble_fn_listOfDates({outputlist1:dates});
+    bubble_fn_listOfDatess({outputlist1:dates});
   }
 
   function convertDatesToTimezone(
